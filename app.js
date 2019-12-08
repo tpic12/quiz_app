417ccb5a8f120284eec264dc20aa00dec233be04
Bilbertius <lauchlinmacneil@gmail.com>
2019-12-08 15:26:20 -0700

    Add styling, results screen, correct answer display, and restart

--- app.js
+++ app.js
@@ -8,52 +8,27 @@ const STORE = {
   questions: [
     {
       question: 'How many items in a bakers dozen??',
-      answers: [
-        '11',
-        '12',
-        '13',
-        '3'
-      ],
+      answers: ['11', '12', '13', '3'],
       correctAnswer: '13'
     },
     {
       question: 'What month is Oktoberfest held?',
-      answers: [
-        'September',
-        'October',
-        'Every Month',
-        'August'
-      ],
+      answers: ['September', 'October', 'Every Month', 'August'],
       correctAnswer: 'September'
     },
     {
       question: 'What color is an Orange Blossom?',
-      answers: [
-        'Red',
-        'Orange',
-        'Pink',
-        'White'
-      ],
+      answers: ['Red', 'Orange', 'Pink', 'White'],
       correctAnswer: 'White'
     },
     {
       question: 'Where did the Panama Hat originate?',
-      answers: [
-        'Ecuador',
-        'Panama',
-        'China',
-        'Cuba'
-      ],
+      answers: ['Ecuador', 'Panama', 'China', 'Cuba'],
       correctAnswer: 'Ecuador'
     },
     {
       question: 'What are toy marbles made from?',
-      answers: [
-        'Plastic',
-        'Quartz',
-        'Diamond',
-        'Glass'
-      ],
+      answers: ['Plastic', 'Quartz', 'Diamond', 'Glass'],
       correctAnswer: 'Glass'
     },
     {
@@ -68,28 +43,18 @@ const STORE = {
     },
     {
       question: 'What is the biggest animal to ever live?',
-      answers: [
-        'Mammoth',
-        'T-Rex',
-        'Blue Whale',
-        'Brontosaurus'
-      ],
+      answers: ['Mammoth', 'T-Rex', 'Blue Whale', 'Brontosaurus'],
       correctAnswer: 'Blue Whale'
     },
     {
       question: 'Where is the red-bellied woodpecker predominately red?',
-      answers: [
-        'Head',
-        'Wings',
-        'Feet',
-        'Belly'
-      ],
+      answers: ['Head', 'Wings', 'Feet', 'Belly'],
       correctAnswer: 'Head'
-    },
+    }
   ],
   questionNumber: 0,
   score: 0,
-  quizStart: false,
+  quizStart: false
 };
 
 /**
@@ -106,22 +71,23 @@ const STORE = {
  */
 
 /**
-  * This function renders the quiz after you start, ...
-  * answer a question, or restart the quiz
-  */
+ * This function renders the quiz after you start, ...
+ * answer a question, or restart the quiz
+ */
 function renderStartQuiz() {
-  const mainCard = 
-    `<section class="main_card">
+  const mainCard = `<section class="main_card">
       <h2>Ready to Start?</h2>
       <button type='submit' id='start_quiz'>Let's Go</button>
     </section>`;
 
   $('.display').html(mainCard);
-  
+
   $('.display').on('click', '#start_quiz', event => {
     event.preventDefault();
     STORE.quizStart = true;
-    $('.main_card').children().remove();
+    $('.main_card')
+      .children()
+      .remove();
     handleQuiz();
   });
 }
@@ -129,31 +95,81 @@ function renderStartQuiz() {
  * renders question one at a time depending on question number
  */
 function renderQuestion() {
-  const qCard = 
-  `<h2>${STORE.questions[STORE.questionNumber].question}</h2>
-  <form id="qForm">
-      <input type='radio' name='answer' id='q1' value="${STORE.questions[STORE.questionNumber].answers[0]}"/><label for='q1' id='0'>${STORE.questions[STORE.questionNumber].answers[0]}</label>
-      <input type='radio' name='answer' id='q2' value="${STORE.questions[STORE.questionNumber].answers[1]}"/><label for='q2' id='1'>${STORE.questions[STORE.questionNumber].answers[1]}</label><br>
-      <input type='radio' name='answer' id='q3' value="${STORE.questions[STORE.questionNumber].answers[2]}"/><label for='q3' id='2'>${STORE.questions[STORE.questionNumber].answers[2]}</label>
-      <input type='radio' name='answer' id='q4' value="${STORE.questions[STORE.questionNumber].answers[3]}"/><label for='q4' id='3'>${STORE.questions[STORE.questionNumber].answers[3]}</label><br>
-  </form>
-  <button type='submit' id='submit_answer'>Final Answer</button>`
-  ;
-  $('.main_card').html(qCard);
+  if (
+    STORE.questionNumber === STORE.questions.length &&
+    STORE.questionNumber > 1
+  ) {
+    console.log(
+      `ran results: ${(STORE.questionNumber, STORE.questions.length)}`
+    );
 
-  $('.main_card').on('click', '#submit_answer', event =>{
-    event.preventDefault();
-    checkAnswer();
-    //original next question spot
-    $('.main_card').children().remove();
-    handleQuiz();
-  });
+    renderResults();
+  } else {
+    const qCard = `<h2>${STORE.questions[STORE.questionNumber].question}</h2>
+    <form id="qForm">
+        <input type='radio' name='answer' id='q1' value="${
+          STORE.questions[STORE.questionNumber].answers[0]
+        }"/><label for='q1' id='0'>${
+      STORE.questions[STORE.questionNumber].answers[0]
+    }</label>
+        <input type='radio' name='answer' id='q2' value="${
+          STORE.questions[STORE.questionNumber].answers[1]
+        }"/><label for='q2' id='1'>${
+      STORE.questions[STORE.questionNumber].answers[1]
+    }</label><br>
+        <input type='radio' name='answer' id='q3' value="${
+          STORE.questions[STORE.questionNumber].answers[2]
+        }"/><label for='q3' id='2'>${
+      STORE.questions[STORE.questionNumber].answers[2]
+    }</label>
+        <input type='radio' name='answer' id='q4' value="${
+          STORE.questions[STORE.questionNumber].answers[3]
+        }"/><label for='q4' id='3'>${
+      STORE.questions[STORE.questionNumber].answers[3]
+    }</label><br>
+    </form>
+    <button type='submit' id='submit_answer'>Final Answer</button>`;
+
+    $('.main_card').html(qCard);
+
+    $('.main_card').on('click', '#submit_answer', function(event) {
+      event.preventDefault();
+      checkAnswer();
+      //original next question spot
+
+      $('.main_card')
+        .children()
+        .remove();
+
+      handleQuiz();
+    });
+  }
 }
 /**
  * renders results page
+ 
  */
-function renderResults(){
 
+function generateResults(finalScore, numberOfQuestions) {
+  return `<h2>You scored ${finalScore} out of ${numberOfQuestions}!</h2><button id='restart'>Restart Quiz</button>`;
+}
+
+function renderResults() {
+  console.log(`ran results: ${STORE.questionNumber} ${STORE.questions.length}`);
+
+  let score = STORE.score;
+  let num = STORE.questionNumber;
+
+  $('.main_card').html(generateResults(score, num));
+  $('#restart').click(function(event) {
+    event.preventDefault();
+    console.log('Restarting Quiz');
+
+    STORE.questionNumber = 0;
+    STORE.score = 0;
+
+    renderQuestion();
+  });
 }
 /**
  * this function checks the correctnes of the answer
@@ -161,34 +177,49 @@ function renderResults(){
 function checkAnswer() {
   let form = document.getElementById('qForm');
   let ans = form['answer'];
-  for(let i=0; i<ans.length; i++){
+  for (let i = 0; i < ans.length; i++) {
     //is a button checked
     if (ans[i].checked) {
       //compare value of ans[i] to correct answer
-      if(ans[i].value === STORE.questions[STORE.questionNumber].correctAnswer){
+      if (
+        ans[i].value === STORE.questions[STORE.questionNumber].correctAnswer
+      ) {
         scoreKeeper(true);
-      }
-      else {
+      } else {
         scoreKeeper(false);
       }
     }
   }
 }
-function scoreKeeper(bool){
-  if (bool===true) {
+
+function generateAnswerTooltip(questionAnswer) {
+  return `<div class="tooltip">
+              <span class="tooltiptext">Sorry, the correct answer is ${questionAnswer}</span>
+          </div>`;
+}
+
+function scoreKeeper(bool) {
+  if (bool === true) {
     console.log('plus 1 point');
     STORE.score++;
     $(`.${STORE.questionNumber}`).addClass('green');
+    $('section.main_card');
     console.log('next q!');
     STORE.questionNumber++;
   }
-  if (bool===false) {
+  if (bool === false) {
     console.log('no points for you');
-    $(`.${STORE.questionNumber}`).addClass('red');
+    $(`.${STORE.questionNumber}`)
+      .addClass('red')
+      .addClass('tooltip')
+      .html(
+        generateAnswerTooltip(
+          STORE.questions[STORE.questionNumber].correctAnswer
+        )
+      );
     console.log('next q!');
     STORE.questionNumber++;
   }
-  
 }
 
 function renderMidScreen(bool) {
@@ -198,12 +229,11 @@ function renderMidScreen(bool) {
   if (bool === true) {
     $('.main_card').html(midCard);
     $('.main-card').addClass('green');
-  }
-  else {
+  } else {
     $('.main_card').html(midCard);
     $('.main-card').addClass('red');
   }
-  $('.main_card').on('click', '#start_button', event =>{
+  $('.main_card').on('click', '#start_button', function(event) {
     event.preventDefault();
     handleQuiz();
   });
@@ -212,32 +242,25 @@ function renderMidScreen(bool) {
  * This function submits the answer to score which
  * updates the render function to render a new question, cannot skip q's
  */
-function submitAnswer() {
-
-}
+function submitAnswer() {}
 /**
  * This function keeps track of the score
  */
-function handleScore() {
-
-}
+function handleScore() {}
 /**
  * This function restarts the quiz
  */
-function restartQuiz() {
-
-}
+function restartQuiz() {}
 /**
  * this function calls all the other code
  */
 function handleQuiz() {
   if (STORE.quizStart === false) {
     renderStartQuiz();
-  }
-  else {
+  } else {
     renderQuestion();
     handleScore();
-    renderResults();
+    //renderResults();
     restartQuiz();
   }
 }
